9f8454b7b56c1dd994d9aedf47f614d083933643
Thomas <thomas@craftacademy.se>
2020-12-09 11:06:58 +0100

    hello world from state in component as an object- version 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import React, { useState } from 'react'
 
 const App = () => {
   // let message = "Hello World from variable"
-  const [message, setMessage] = useState('Hello World from hooked state')
+  const [message, setMessage] = useState({ greeting: 'Hello World from hooked state using an object' })
   return (
     <>
-      <h1>{message}</h1>
+      <h1>{message.greeting}</h1>
     </>
   )
 }
